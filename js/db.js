e67213fddd7c37f712e26051ac2d97342dab5560
Serdar <udemuradovv@gmail.com>
2024-02-05 00:09:04 +0500

    first commit

---
+++ js/db.js
@@ -0,0 +1,118 @@
+var arrLang = {
+  'ru': {
+    'home': 'Главная',
+    'about': 'О нас',
+    'products': 'Продукция',
+    'contacts': 'Контакты',
+    'gallery': 'Галерея',
+    'russian': 'Русский',
+    'turkmen': 'Туркменский',
+    'english': 'Английский',
+    'active_lang': 'Русский',
+
+    'main_title': 'Производство<br>Гипохлорит Натрия',
+    'main_subtitle': 'Гипохлорит натрия изготавливается в соответствии с утвержденным технологическим регламентом и соответствует требованиям стандартов Туркменистана.',
+
+    'about_title': 'Хозяйственное общество «Даг Дамжасы» — местное производственное предприятие, расположенное в Балканском велаяте Туркменистана, выпускающее высококачественные растворы гипохлорита натрия концентрацией до 15%. Продукция изготавливается в соответствии с утвержденным технологическим регламентом и соответствует требованиям стандарта Туркменистана и доступна для потребителей 24/7.',
+    'about_achiv_1': 'Продукт сертифицирован<br>по местному стандарту TDS',
+    'about_achiv_2': 'Высококачественные<br>растворы гипохлорита натрия',
+    'about_achiv_3': 'Квалифицированные<br>сотрудники',
+
+    'products_title': 'О продукции',
+    'products_subtitle': 'Гипохлорит Натрия - дезинфицирующее, детоксицирующее, антисептическое, противомикробное средство. Применяется для предотвращения и устранения развития биологических загрязнений в оборотно-охлаждающих системах, обеззараживании питьевой воды, домов (стен, плинтусов), мебели, посуды, санитарно-технического оборудования, гигиенических материалов, изделий медицинского назначения, а также обеззараживания воды в плавательных бассейнах',
+    'products_achiv_1': 'подходит для обработки офисов и домов (стен, плинтусов), мебели, посуды',
+    'products_achiv_2': 'обеззараживает изделия медицинского назначения',
+    'products_achiv_3': 'обеззараживает питьевую воду, а также воду в плавательных бассейнах',
+    'products_achiv_4': 'предотвращает развитие биологических загрязнений в оборотно-охлаждающих системах',
+
+    'address_title': 'Адрес:',
+    'address_name': 'Туркменбаши этрап, улица Асудалык<br>строение 14 Туркменбаши/Туркменистан',
+    'contact_us': 'Свяжитесь с нами',
+    'form_name': 'Имя',
+    'form_email': 'Email',
+    'form_phone': 'Номер телефона',
+    'form_message': 'Сообщение',
+    'form_send': 'Отправить заявку',
+
+    'footer_privacy': 'И.П. "Dag Damjasy" © 2024. Все Права Защишены'
+  },
+
+
+  'tm': {
+    'home': 'Baş sahypa',
+    'about': 'Biz barada',
+    'products': 'Hyzmatlarymyz',
+    'gallery': 'Galereýa',
+    'contacts': 'Habarlaşmak üçin',
+    'russian': 'Rusça',
+    'turkmen': 'Türkmençe',
+    'english': 'English',
+    'active_lang': 'Türkmençe',
+
+    'main_title': 'Önümçilik<br>Natriý Gipohloridi',
+    'main_subtitle': 'Natriý gipohloridi bellenilen tertipde tassyklanan tehnologiki reglamentlere laýyklykda taýýarlanylýar Türkmenistanyň standartyň talaplaryna laýyk gelýär.',
+
+    'about_title': '"Dag Damjasy", Türkmenistanyň Balkan sebitinde ýerleşýän ýerli önümçilik kärhanasy bolup, 15% vol konsentrasiýasy bilen natriý gipohloritiň ýokary hilli erginlerini öndürýär. Önümler ýerli çig maldan öndürilýär, Türkmenistan standartyna laýyklykda kepillendirilýär we sarp edijiler üçin gije-gündiziň dowamynda elýeterlidir.',
+    'about_achiv_1': 'Ýerli TDS standartyna<br>laýyklykda kepillendirilýär',
+    'about_achiv_2': 'Natriý gipohloritiň<br>ýokary hilli erginlerini öndürýär',
+    'about_achiv_3': 'Kwalifikasiýaly<br>işgärler',
+
+    'products_title': 'Önümler hakda',
+    'products_subtitle': 'Natriý gipohlorit dezinfeksiýa, detoksifikasiýa, antiseptik, mikroblara garşy serişdedir. Aýlawly-sowatmak sistemalaryň biologik zyýanlarynyň ösüşini saklamak we aýyrmak, agyz suwy zyýansyzlandyrmak, jaýlaryň (diwarlarynyň, eteginiň), mebelleriň, gap-çanaklaryň, arassaçylyk-tehniki enjamlaryň, arassaçylyk materiallaryň, medisina maksatlary üçin niýetlenen önümleriň, şeýle-de howuzlardaky (basseýn) suwy zyýansyzlandyrmak işlerini geçirmeklikde ulanylýar.',
+    'products_achiv_1': 'ofisleriň we jaýlaryň (diwarlarynyň, eteginiň), mebelleriň, gap-çanaklaryň gaýtadan işlemek üçin amatly',
+    'products_achiv_2': 'Medisina önümlerini dezinfeksiýa edýär',
+    'products_achiv_3': 'agyz suwy we howuzlardaky (basseýn) suwy zyýansyzlandyrmak işlerini geçirmeklikde ulanylýar',
+    'products_achiv_4': 'Aýlawly-sowatmak sistemalaryň biologik zyýanlarynyň ösüşini saklamak we aýyrmak',
+
+    'address_title': 'Adres:',
+    'address_name': 'Türkmenbaşy etrap, Asudalyk köçesi 14 <br>  Türkmenbaşy / Türkmenistan',
+    'contact_us': 'Biz bilen habarlaşyň',
+    'form_name': 'At',
+    'form_email': 'Email',
+    'form_phone': 'Telefon belgi',
+    'form_message': 'Hat',
+    'form_send': 'Iber',
+    'footer_privacy': 'H.K. "Dag Damjasy" © 2024. Ähli hukuklar goralan.'
+  },
+
+
+
+
+
+  'en': {
+    'home': 'Home',
+    'about': 'About',
+    'products': 'Products',
+    'gallery': 'Gallery',
+    'contacts': 'Contacts',
+    'russian': 'Russian',
+    'turkmen': 'Turkmen',
+    'english': 'English',
+    'active_lang': 'English',
+    'main_title': 'Production of<br> Sodium Hypochlorite',
+    'main_subtitle': 'Sodium hypochlorite is manufactured in accordance with approved technological regulations and meets the requirements of Turkmenistan standards.',
+
+    'about_title': 'Economic Society "Dag Damzhasy" is a local manufacturing enterprise located in the Balkan region of Turkmenistan, producing high-quality solutions of sodium hypochlorite with a concentration of up to 15%. The products are manufactured in accordance with the approved technological regulations and meet the requirements of the Turkmenistan standard and are available to consumers 24/7.',
+    'about_achiv_1': 'Product certified<br>to local TDS standard',
+    'about_achiv_2': 'High quality<br>sodium hypochlorite solutions',
+    'about_achiv_3': 'Qualified<br>employees',
+
+    'products_title': 'About products',
+    'products_subtitle': 'Sodium hypochlorite is a disinfectant, detoxifying, antiseptic, antimicrobial agent. It is used to prevent and eliminate the development of biological contaminants in circulating cooling systems, disinfection of drinking water, houses (walls, baseboards), furniture, dishes, sanitary equipment, hygiene materials, medical products, as well as disinfection of water in swimming pools.',
+    'products_achiv_1': 'suitable for processing offices and homes (walls, baseboards), furniture, dishes',
+    'products_achiv_2': 'disinfects medical products',
+    'products_achiv_3': 'disinfects drinking water and water in swimming pools',
+    'products_achiv_4': 'prevents the development of biological contaminants in circulating cooling systems',
+
+    'address_title': 'Address:',
+    'address_name': 'Turkmenbashi etrap, Asudalyk street<br>building 14 Turkmenbashi/Turkmenistan',
+    'contact_us': 'Contact us',
+    'form_name': 'Name',
+    'form_email': 'Email',
+    'form_phone': 'Phone number',
+    'form_message': 'Message',
+    'form_send': 'Send',
+
+    'footer_privacy': 'ES "Dag Damjasy" © 2024. All rights reserved.'
+  }
+};

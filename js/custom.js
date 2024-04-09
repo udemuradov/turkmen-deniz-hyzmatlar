14acc8f01858481e8c2da183e8fca4e473ce238e
su <udemuradovv@gmail.com>
2024-04-09 12:15:57 +0500

    bugfix

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,7 @@ if (cookLang) {
 } else {
     setCookie('lang', defLang);
 }
+
 function changeLang(lang) {
     console.log('defLang: ', defLang)
     console.log('lang: ', lang)
@@ -24,24 +25,23 @@ function changeLang(lang) {
 
 
     let loc = window.location.pathname;
-    var blackLogo = document.getElementById("blacklogo");
-    var whiteLogo = document.getElementById("whitelogo");
-    var tableInfo = document.getElementById("tableinfo");
-    var aboutViewMore = document.getElementById("aboutviewmorebtn");
+    var aboutViewMore = document.getElementById("");
     var formName = document.getElementById('formname');
     var formNumber = document.getElementById('formnumber');
     var formMessage = document.getElementById('formmessage');
     if (loc == '/' || loc == '/index.html' || loc == '/dag-damjasy/') {
         if (lang == 'ru') {
-            tableInfo.setAttribute('src', 'images/table.svg');
+
         }
         if (lang == 'tm') {
-            tableInfo.setAttribute('src', 'images/table-tm.svg');
 
         }
         if (lang == 'en') {
-            tableInfo.setAttribute('src', 'images/table-en.svg');
 
         }
     }
+}
+
+function reload(){
+    location.reload();
 }
